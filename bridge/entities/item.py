fd43286892e438f4f778e7e1dc5e0d591fad04e4
Morgan Aubert <morgan.aubert@impakfinance.com>
2018-11-20 10:53:14 -0500

    Added additional item-related endpoints

--- bridge/entities/item.py
+++ bridge/entities/item.py
@@ -21,3 +21,32 @@ class Item(BaseApi):
 
         """
         return self._client._call('GET', 'items/add/url')
+
+    def get(self, id):
+        """ Retrieves the details of a single item.
+
+        :param id: ID of the considered item
+        :type id: str or int
+        :return: dictionary containing the item details
+        :rtype: dictionary
+
+        """
+        return self._client._call('GET', 'items/{}'.format(id))
+
+    def list(self, before=None, after=None, limit=None):
+        """ List the items associated with the current user.
+
+        :param before: cursor pointing to the end of the desired set
+        :param after: cursor pointing to the start of the desired set
+        :param limit: number of records to return (accepted values: 1 - 500)
+        :type before: str
+        :type after: str
+        :type limit: int
+        :return: dictionary containing the bank accounts
+        :rtype: dictionary
+
+        """
+        params = {'before': before, 'after': after, 'limit': limit, }
+        return self._patch_paginated_response_data(self._client._call(
+            'GET', 'items', params={k: v for k, v in params.items() if v is not None},
+        ))
